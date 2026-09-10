a28622ae5bf9a541f0933961895db56b71a42aa7
nabilatasmayeni-dotcom <nabilatasmayeni@gmail.com>
2026-09-10 14:54:35 +0700

    Add files via upload

---
+++ pekan1/Tugas1_2611533014.py
@@ -0,0 +1,13 @@
+
+print("               BIODATA MAHASISWA                  ")
+print("==================================================")
+print("Nama Lengkap    : Nabila Tasmayeni")
+print("NIM             : 2611533014")
+print("Fakultas        : Teknologi Informasi")
+print("Departemen      : Informatika")
+print("Universitas     : Universitas Andalas")
+print("Tahun Angkatan  : 2026")
+print("Mata Kuliah     : Praktikum Algoritma & Pemrograman")
+
+print("Status Program  : Berhasil Dijalankan")
+print("==================================================")
