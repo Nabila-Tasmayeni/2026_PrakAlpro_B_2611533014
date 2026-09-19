4a0221c780de5a88381795313949c91b135f2960
Nabila-Tasmayeni <nabilatasmayeni@gmail.com>
2026-09-19 11:00:17 +0700

    Add files via upload

---
+++ pekan3/program3/bitwise_2611533014.py
@@ -0,0 +1,58 @@
+#Buat file dengan nama Bitwise_NIM.py
+#Nama variabel ditambah 4 digit nim terakhir contoh: angka1_1234
+#Program ini menggunakan fungsi input()
+
+print("\n==================================")
+print("3. OPERATOR BITWISE")
+print("===================================")
+
+angka1_3014 = int(input("Masukkan angka bitwise-1:"))
+angka2_3014 = int(input("Masukkan angka bitwise-2:"))
+
+print("\nAngka dalam bentuk desimal dan biner")
+print("angka1_3014 =", angka1_3014, "| biner =", bin(angka1_3014))
+print("angka2_3014 =", angka2_3014, "| biner =", bin(angka2_3014))
+
+#Bitwise AND
+hasil_3014 = angka1_3014 & angka2_3014
+print("\nBitwise AND (&)")
+print("angka1_3014 & angka2_3014 =", hasil_3014)
+print("Biner hasil =", bin(hasil_3014))
+print("Biner hasil (8 bit) =", format(hasil_3014, '08b'))
+
+#Bitwise OR
+hasil_3014 = angka1_3014 | angka2_3014
+print("\nBitwise OR (|)")
+print("angka1_3014 | angka2_3014 =", hasil_3014)
+print("Biner hasil =", bin(hasil_3014))
+print("Biner hasil (8 bit) =", format(hasil_3014, '08b'))
+
+#Bitwise XOR
+hasil_3014 = angka1_3014 ^ angka2_3014
+print("\nBitwise XOR (^)")
+print("angka1_3014 ^ angka2_3014 =", hasil_3014)
+print("Biner hasil =", bin(hasil_3014))
+print("Biner hasil (8 bit) =", format(hasil_3014, '08b'))
+
+#Bitwise NOT
+hasil_3014 = ~angka1_3014
+print("\nBitwise NOT (~)")
+print("~angka1_3014 =", hasil_3014)
+print("Biner hasil =", bin(hasil_3014))
+print("Biner hasil (8 bit) =", format(hasil_3014, '08b'))
+
+#Bitwise geser ke kiri
+jumlah_geser_3014 = int(input("\nMasukkan jumlah pergeseran bit: "))
+
+hasil_3014 = angka1_3014 << jumlah_geser_3014
+print("\nBitwise geser ke kiri (<<)")
+print("angka1_3014 <<", jumlah_geser_3014, "=", hasil_3014)
+print("Biner hasil =", bin(hasil_3014))
+print("Biner hasil (8 bit) =", format(hasil_3014, '08b'))
+
+#Bitwise geser kanan
+hasil_3014 = angka1_3014 >> jumlah_geser_3014
+print("\nBitwise geser kanan (>>)")
+print(angka1_3014, ">>", jumlah_geser_3014, "=", hasil_3014)
+print("Biner hasil =", bin(hasil_3014))
+print("Biner hasil (8 bit) =", format(hasil_3014, "08b"))
---
+++ pekan3/program3/logika_2611533014.py
@@ -0,0 +1,37 @@
+#Buat file dengan nama logika_NIM.py
+#Nama variabel ditambah 4 digit nim terakhir contoh : angka_1234
+#Program ini menggunakan fungsi input()
+#Program operator Logika dalam Python
+
+#Masukkan nilai boolean
+#Input tidak peka terhadap huruf besar dan kecil 
+a1_3014 = input ("Input nilai boolean-1(True/False): "). strip().lower() == "true"
+a2_3014 = input ("Input nilai boolean-2(True/False): "). strip().lower() == "true"
+
+print("\nA1 =", a1_3014)
+print("A2 =" , a2_3014)
+
+#Konjugasi : bernilai True jika keduanya True
+hasil_3014 = a1_3014 and a2_3014
+print("\nKonjugasi (AND)")
+print("A1_3014 and A2_3014 =", hasil_3014)
+
+#Disjungsi: bernilai True jika salah satunya True
+hasil_3014 = a1_3014 or a2_3014
+print("\nDisjungsi (OR)")
+print("A1_3014 or A2_3014 =", hasil_3014)
+
+#Negasi A1_3014: membalik nilai A1_3014
+hasil_3014 = not a1_3014
+print("\nNegasi A1_3014 (NOT)", hasil_3014)
+print("not A1_3014 =", hasil_3014)
+
+#Negasi A2_3014: membalik nilai A2_3014
+hasil_3014 = not a2_3014
+print("\nNegasi A2_3014 (NOT)")
+print("not A2_3014 =", hasil_3014)
+
+#XOR: bernilai True jika kedua nilai berbeda
+hasil_3014 = a1_3014 != a2_3014
+print("\nDisjungsi Eksklusif (XOR)")
+print("A1_3014 XOR A2_3014 =", hasil_3014)
